85e1a06e096ae7bb7c1fa228963b086995836941
Vasiliy Klimkin <vasily.klimkin@gmail.com>
2019-10-16 11:22:46 -0400

    :books: Added Batman function

--- w01d03-Objects-in-JS/demo2.js
+++ w01d03-Objects-in-JS/demo2.js
@@ -24,4 +24,75 @@ let dogCatCounter = function(string) {
   return obj;
 };
 
-console.log('There are:   ',dogCatCounter(str));
+// console.log('There are:   ',dogCatCounter(str));
+
+
+// let a = 1;
+// let b = 4;
+let obj2 = {
+  a: 1,
+  b: 4,
+}
+
+
+const swap = function(obj) {
+  let temp = obj.a;
+  obj.a = obj.b;
+  obj.b = temp;
+}
+let copyObj = {
+  a: obj2.a,
+  b: obj2.b
+}
+
+copyObj.a = 72;
+
+// console.log('A AND B BEFORE:', obj2);
+
+swap(obj2);
+
+// console.log('A and B after -->', obj2);
+
+
+
+
+let batman = {
+  realName: 'Bruce Wayne',
+  suit: 'Mark 2',
+  gadgets: []
+}
+// Make a function that adds a new gadget
+// to the the batman object. The batman object
+// will be passed into our function..
+const addGadget = function(gadgetName, obj) {
+  // console.log(gadgetName, obj);
+  // console.log(obj.gadgets)
+  obj.gadgets.push(gadgetName);
+}
+
+
+console.log("BATMAN before function", batman);
+addGadget('Grapling Hook', batman);
+addGadget('Grapling Hook', batman);
+addGadget('Grapling Hook', batman);
+addGadget('Grapling Hook', batman);
+addGadget('Grapling Hook', batman);
+addGadget('Grapling Hook', batman);
+addGadget('Grapling Hook', batman);
+addGadget('Grapling Hook', batman);
+addGadget('Smoke Granade', batman);
+addGadget('Grapling Hook', batman);
+addGadget('Grapling Hook', batman);
+addGadget('Grapling Hook', batman);
+addGadget('Grapling Hook', batman);
+addGadget('Grapling Hook', batman);
+addGadget('Grapling Hook', batman);
+console.log("BATMAN after  function", batman);
+
+
+
+
+
+
+
+
