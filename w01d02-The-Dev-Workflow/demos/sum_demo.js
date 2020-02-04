fa9235a22ae2528b7d666f7a499e02b33f206510
Vasiliy Klimkin <vasily.klimkin@gmail.com>
2020-02-04 11:08:48 -0500

    figured out how to get args from node

---
+++ w01d02-The-Dev-Workflow/demos/sum_demo.js
@@ -0,0 +1,16 @@
+// I want to take node arguments
+// ( they have to be numbers )
+// and then I want to add each number and return the sum
+// I want to ignore anything thats not a whole number
+
+// take node arguments (WELL WHAT IS THAT???!?!)
+// how do i check if number??
+// might use an array?
+// use a loop
+// how to check if its a whole number???
+
+let arr = process.argv.slice(2);
+console.log('arr => ', arr);
+
+
+// console.log(process.argv)
