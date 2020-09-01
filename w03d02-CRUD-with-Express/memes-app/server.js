9cd07e4d4ad4b2087f4c8fb93a430bbd9d79c783
vasiliy-klimkin <vasily.klimkin@gmail.com>
2020-09-01 12:21:43 -0400

    :books: new live lecture added!

--- w03d02-CRUD-with-Express/memes-app/server.js
+++ w03d02-CRUD-with-Express/memes-app/server.js
@@ -1,88 +1,81 @@
-const PORT = 8080;
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const morgan = require('morgan')
+const morgan = require('morgan');
 
-app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }))
-
-let key = 3;
-
-const memeList = {
-  '0': {
-    img: 'https://preview.redd.it/i3gm65ks3u351.jpg?width=640&crop=smart&auto=webp&s=a0acd2afaf7f3087d8624aefe984888e1ba6e027',
-    name: 'First meme'
-  },
-  '1': {
-    img: 'https://preview.redd.it/3f5dxbjjau351.jpg?width=640&crop=smart&auto=webp&s=311d94677a9c6c97cd0d95377bfd8e9290821f4a',
-    name: 'bug meme 2nd'
-  },
-  '2': {
-    img: 'https://external-preview.redd.it/nHoYJcin0lEe_WlBu71a2ayXRvHl9_Z8msF6VOuNOds.jpg?width=640&crop=smart&auto=webp&s=e200ea5218d8dbab603d309d3c3aed6abd71f495',
-    name: 'third meme'
-  },
-}
+app.use(morgan('dev'));
 
 app.set('view engine', 'ejs');
-// People mix these words 
-// this causes big issues, be careful!!!
+
+let number = 5; // Cheat-sy way of making a custom key
+
+// our "database" of items
+const memesObj = {
+    '1': 'https://i.redd.it/isljlhhl3dk51.jpg',
+    '2': 'https://preview.redd.it/d6lpkz8qfik51.png?width=640&crop=smart&auto=webp&s=e2e462e04b636f90c1f623aa83d4c2c2ad61afe9',
+    '3': 'https://preview.redd.it/o284ez9yrck51.jpg?width=640&crop=smart&auto=webp&s=3f9c873ef0d478d889acebb4f535a142513a4e26',
+    '4': 'https://preview.redd.it/a9yjtkex74k51.png?width=640&crop=smart&auto=webp&s=c9bfe51fee74d43e2d2f58e41ab7bbeaa456ea91'
+}
+
+// first param is URL requested
 app.get('/', (req, res) => {
-    res.render('home_page');
-  })
-  // CRUD
-  // C - Create
-  // R - Read
-  // U - Update
-  // D - Delete
 
-// REST-ful Convetion
-// provide a pattern that allows easy data manipulation for CRUD 
-// Developers name their routes in such a way that everything will have consistency.
-
-// GET /urls ===> displays every single URL 
-// GET /memes getting a list of memes to appear on the page!!
-app.get('/memes', (req, res) => {
-  const templateVars = { list: memeList }
-  res.render('memes', templateVars);
+    res.render('home');
 })
+// a server must be able to 
+// Create, Read, Update, Delete some sort of Data for it function
 
-// PITFALL -- order matters sometimes!! 
-// need to put this before /:id!
+
+
+app.get('/memes', (req,res) => {
+    const templateVars = {memes: memesObj}
+    console.log('/memes route');
+    // first thing pass templateVars OBJ as a second parameter
+    res.render('memes_index', templateVars);
+})
+// The RESTFUL convention!!!
+
+// GET /memes/new
 app.get('/memes/new', (req, res) => {
-  res.render('memes_create');
+    console.log("memes/new route");
+    res.render('memes_new');
 })
 
-
-// IF i want the id from URL i will use req.params
-app.get('/memes/:id', (req, res) => {
-    const id = req.params.id;
-    const meme = memeList[id];
-    const templateVars = { meme: meme }
-    res.render('memes_show', templateVars)
-  })
-  // if i wanted to edit a meme my route would be...
-  // PUT OR
-  // POST /memes/:id
-
+// GET /memes/id 
+app.get('/memes/:meme_id', (req, res) => {
+    console.log('/memes/:id route');
+    console.log(req.params.meme_id); // <--- this is a key of my meme database
+     // memesObj <-- database of memes 
+    // console.log(memesObj[req.params.meme_id]);
+    const templateVars = {
+        memeImg: memesObj[req.params.meme_id]
+    }
+    res.render('memes_show', templateVars);
+    // res.send('this supposed to be a single meme');
+})
 
 // POST /memes
-// req.params <--- WAS ONLY for URL for the top like :id
-// req.body (body-parser) <--- IS FOR some information that was filled out by the end user/client
-// aka: form input
 app.post('/memes', (req, res) => {
-  // info coming from the form
-  let tempMeme = { img: req.body.meme_url, name: req.body.meme_name };
-  memeList[key] = tempMeme;
-  key++;
-  //^fake Uid generator
-
-  res.redirect('/memes');
+    console.log("we are about to save a meme!!!!");
+    console.log(req.body);
+    // take what info user has sent me 
+    // and store it in my object
+    // using the "number" variable im going to store the value into my 
+    // memeObj
+    memesObj[number] = req.body.newMemeURL;
+    number++;
+    // once im done im going to do ???
+    res.redirect('/memes');
+    // vs res.render()
 })
+// /u/:shortURL  =====> res.redirect('https://____________')
+
+// DELETE
+//  POST /memes/:id/delete
+//  GET /memes/:id/edit <---- GET THE FORM
+//  POST /memes/:id     <--- actually saving 
 
 
-// IF i want to delete a meme
-// what would be my route?
-// METHOD: POST /memes/:id/delete or DELETE /memes/:id
 
-app.listen(PORT, () => console.log('App is working on port ' + PORT));
+app.listen(8080, () => console.log('Server is listening on port 8080'));
