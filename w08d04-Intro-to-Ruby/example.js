7539e221c01902a06a9a1fc227621ee9078fab89
vasiliy-klimkin <vasily.klimkin@gmail.com>
2020-10-09 13:29:23 -0400

    :books: live lecture added

---
+++ w08d04-Intro-to-Ruby/example.js
@@ -0,0 +1,7 @@
+const hello = (name) => {
+    console.log(`hello ${name}`);
+}
+
+const obj = {a: 1, b:2, c: 'three'}
+
+hello(12);
---
+++ w08d04-Intro-to-Ruby/example.rb
@@ -0,0 +1,21 @@
+
+# def he
+    
+
+# hello(12)
+
+arr = (1..10)
+# hash = { a: 1, 'b' => 2 }
+# hash[:c] = 'three'
+# puts hash[:a]
+
+# arr = [1,2,3,4,5,6,7,8,9,10]
+# arr.forEach(number => ...)
+newArr = arr.map do |number|
+    number * 2
+end
+
+# a = hello('vas')
+# puts a
+
+# puts 'this is pritning' if true
