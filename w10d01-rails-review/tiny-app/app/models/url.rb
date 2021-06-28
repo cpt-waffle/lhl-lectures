a1ae1d547485c8016460e38452b17c29cb36459e
vasiliy-klimkin <vasily.klimkin@gmail.com>
2021-06-28 09:41:40 -0400

    :books: deleting old example

--- w10d01-rails-review/tiny-app/app/models/url.rb
+++
@@ -1,8 +0,0 @@
-class Url < ApplicationRecord
-    belongs_to :user
-end
-
-# Order.find(params[:id])
-# @order.line_items.each |item| do 
-#    item.product 
-# end 
--- w10d01-rails-review/tiny-app/config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  # rails generates you a RESTFUL convention routes, based on the the word you give
-  resources :urls, only: [:index, :new, :create, :destroy]
-  # make an abouts_page ?
-  # app.use('/about', pagesRoutes(db))
-  get '/about', to: 'pages#about'
-  get '/teams', to: 'pages#teams'
-
-end
