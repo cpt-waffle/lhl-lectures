f64d63cdc7f1c9720dd2d73e44c0bfab932d6bd5
vasiliy-klimkin <vasily.klimkin@gmail.com>
2021-11-17 12:51:36 -0500

    :books: live lecture added

--- w04d03-Ajax/demo2/public/scripts/app.js
+++ w04d03-Ajax/demo2/public/scripts/app.js
@@ -1,121 +1,163 @@
-console.log("app.js");
-
-const dummyObj = {
-    "score": 0.894875,
+// I need to figure out a way to send a request to the API
+  // JQUERY
+  // GET  /tweets
+  // POST /tweets
+const dummyShowObject = {
+    "score": 0.6971376,
     "show": {
-        "id": 52361,
-        "url": "https://www.tvmaze.com/shows/52361/cars",
-        "name": "Cars",
-        "type": "Animation",
-        "language": "English",
-        "genres": [],
-        "status": "In Development",
-        "runtime": null,
-        "averageRuntime": null,
-        "premiered": null,
-        "officialSite": null,
+        "id": 44579,
+        "url": "https://www.tvmaze.com/shows/44579/top-dog",
+        "name": "Top Dog",
+        "type": "Scripted",
+        "language": "Swedish",
+        "genres": [
+            "Drama",
+            "Crime",
+            "Thriller"
+        ],
+        "status": "To Be Determined",
+        "runtime": 45,
+        "averageRuntime": 45,
+        "premiered": "2020-10-07",
+        "ended": null,
+        "officialSite": "https://www.cmore.se/serie/148290-top-dog",
         "schedule": {
-            "time": "",
-            "days": []
+            "time": "21:00",
+            "days": ["Wednesday"]
         },
         "rating": {
-            "average": 4.4
+            "average": null
         },
-        "weight": 0,
-        "network": null,
+        "weight": 44,
+        "network": {
+        "id": 1367,
+        "name": "C More",
+        "country": {
+            "name": "Sweden",
+            "code": "SE",
+            "timezone": "Europe/Stockholm"
+        }
+        },
         "webChannel": {
-            "id": 287,
-            "name": "Disney+",
-            "country": null
+            "id": 170,
+            "name": "C More",
+            "country": {
+                "name": "Sweden",
+                "code": "SE",
+                "timezone": "Europe/Stockholm"
+            }
         },
         "dvdCountry": null,
         "externals": {
             "tvrage": null,
-            "thetvdb": null,
-            "imdb": null
+            "thetvdb": 377684,
+            "imdb": "tt11542694"
         },
-        "image": null,
-        "summary": "<p>Pixar is speeding ahead with a new series following Lightning McQueen and Mater on a road trip across the country. Featuring new characters, old friends, and imaginative destinations. Coming to Disney+ in Fall 2022.</p>",
-        "updated": 1607679181,
+        "image": {
+        "medium": "https://static.tvmaze.com/uploads/images/medium_portrait/276/691903.jpg",
+        "original": "https://static.tvmaze.com/uploads/images/original_untouched/276/691903.jpg"
+        },
+        "summary": "<p><b>Top Dog</b> centres on the clash between ambitious business lawyer Emily Jansson and the ex-crook Teddy Maksumic. A mysterious disappearance brings together two completely different worlds, and an extremely ill-matched couple - lawyer Emily and the conditionally released Teddy. The two are at loggerheads. She wants up. He wants out. She wants to secure her future; he wants to leave his old life behind. The pair are forced to rethink when they end up in a tricky situation where the past catches up, where future dreams are shattered, and where nothing is what it seems.</p>",
+        "updated": 1606764898,
         "_links": {
             "self": {
-                "href": "https://api.tvmaze.com/shows/52361"
+            "href": "https://api.tvmaze.com/shows/44579"
+            },
+            "previousepisode": {
+                "href": "https://api.tvmaze.com/episodes/1956412"
             }
         }
     }
 }
 
+const defaultImg = "https://i.pinimg.com/originals/1d/e8/b3/1de8b334143659ea628a7ee620d2f6ab.jpg"
 
-// $.ajax({
-//     url: 'https://api.tvmaze.com/search/shows?q=cars',
-//     success: function(data) {
-//         console.log("completed!");
-//         console.log(data);
-//     },
-//   });
+const addMovie = (movieObj) => {
+    let img = defaultImg;
 
-
-// this line of code, calls an api and grabs us the information we need.
-// the info comes as an array of objects....
-// $.get('https://api.tvmaze.com/search/shows?q=cars').then(data => {
-//     console.log("success");
-//     console.log(data);
-// })
-
-// we want to see a picture
-
-// name, rating/ img
-// title year
-// url
-// genre
-// streaming or playing where
-
-const addShow = (item) => {
-    console.log('-------------------');
-    console.log(item.show)
-    let rating = 0;
-    let image = "https://i.redd.it/6xvh1f7btgl31.jpg";
-
-    if (item.show.rating.average) {
-        rating = item.show.rating.average;
+    if (movieObj.show.image) {
+        img = movieObj.show.image.medium;
     }
 
-    if (item.show.image) {
-        image = item.show.image.original;
-    }
-    $('#shows').append(`
-    <div class="show">
-        <div>
-            <h3 class="show--title">${item.show.name}</h3>
-            <img class="show--img" src="${image}"/>
-            <h4>Rating: ${rating}</h4>
+    $('.movies').append(`
+    <div class="movie">
+        <img class="movie--img" src="${img}"/>
+        <div class="movie--desc">
+            <h1>${movieObj.show.name}</h1>
+            <h2>${movieObj.score}</h2>
+            <h3>${movieObj.show.genres}</h3>
+            <h4>${movieObj.show.language}</h4>
+            <h5>${movieObj.show.premiered}</h5>
         </div>
-        <div class="show--desc">
-            ${item.show.summary}
-        </div>
+        <p class="movie--summary">
+        ${movieObj.show.summary}
+        </p>
     </div>
     `);
 }
 
-const findShow = (name) => {
-    $('#shows').empty();
-    $.get(`https://api.tvmaze.com/search/shows?q=${name}`).then(data => {
-        console.log("success");
-        for (let item of data) {
-            addShow(item);
-        }
-    })
-}
+// we wannna see
+// rating
+// title
+// genres
+// image
+// country
+// language
+// premiered
 
+// form ----> event listner ------->  preventDefault
 
-$( document ).ready(function() {
-    // findShow('dogs')
-    $('#form').on('submit', (evt) => {
+// evt.target.movieName
+/////////////////////////////////////////////////////////////////////
+// serialize the value 
+// const val = $(evt.target.movieName).serialize();
+//
+// $.post('/tweets/', val).then(() => {
+
+// })
+
+////////////////////////////////////////////////////////////////////
+
+$(() => {
+    let tvMazeApiURL = 'https://api.tvmaze.com/search/shows?q=';
+    /// EVENT LISTNER FOR FORM TO CONTROL THE SEARCH OPTION
+    // add this inside of document.on.ready because just like append()
+    // you can only attach an event listener to a loaded HTML tag
+    $('#search').on('submit', (evt) => {
         evt.preventDefault();
-        const name = evt.target.searchShow.value;
-        // console.log($('#searchShow').val());
-        findShow(name);
+        const val = $(evt.target.movieName).serialize();
+        console.log("VAL--->", val);
+        console.log(evt.target.movieName.value);
+        $('.movies').empty();
+        $.get(tvMazeApiURL + evt.target.movieName.value).then(data => {
+            console.log("what happened?");
+            console.log(data);
+            for (movie of data) {
+              addMovie(movie);
+            }
+          })
     })
 });
 
+
+
+
+
+
+
+
+// HOW do I, add an item FROM app.js to an index.html file ?
+
+  let tvMazeApiURL = 'https://api.tvmaze.com/search/shows?q=dog';
+//   $.get(tvMazeApiURL).then(data => {
+//       console.log("what happened?");
+//       console.log(data);
+//   })
+
+
+// I need to see what sort of data object/array im working with
+
+// I need to build a layout for these data object(s)
+
+// Styling :) 
 
