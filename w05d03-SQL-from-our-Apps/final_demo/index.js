666038c16867648bcc086d132fa2b8c14022b411
vasiliy-klimkin <vasily.klimkin@gmail.com>
2022-01-05 12:44:53 -0500

    :books: live lecture added

--- w05d03-SQL-from-our-Apps/final_demo/index.js
+++ w05d03-SQL-from-our-Apps/final_demo/index.js
@@ -1,32 +1,39 @@
 const PORT = 8080;
-// STEPS ON HOW TO CONNECT POSTGRES DB 
-// INTO YOUR NODEJS EXPRESSJS APPLICATION
 
-// 1 -- make pool, give parameters to pool
-// 2 (optional) -- pool.connect()
-// 3 -- make queries and send em to the views as array
+//    STEPS TO CONNECT PG (psql) into express server
+
+// require(pg) - use Pool
+
+// new Pool({})
+
+// connect() <---- optional
+
+// pool.query()--->
 
 
+
+
+const {Pool, Client} = require('pg');
 const bodyParser	= require('body-parser');
 const express			= require('express');
 const morgan			= require('morgan');
-const { Pool } = require('pg');
 const app = express();
 
+// pool credentials <----
 const pool = new Pool({
     user: 'labber',
     password: 'labber',
-    port: '5432',
     host: 'localhost',
-    database: 'w05d03'
+    database: 'w05d03',
+    port: '5432'
 });
 
+// show me if connects....
 pool.connect().then(() => {
-    console.log("We have connected to our database :)");
+    console.log('DATABASE STATUS: Connected :)');
 }).catch(e => {
-    console.log('----------- Error -----------');
-    console.log(e.message);
-});
+    console.log(e);
+})
 
 
 
@@ -34,65 +41,88 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(morgan('dev'));
 app.set('view engine', 'ejs');
 
+
+let people = 1;
+
+app.get('/cats', (req, res) => {
+	people++;
+	const templateVars = {name: 'Vas', count: people};
+	const command = 'SELECT * FROM addresses WHERE id = $1';
+	const values = [ 3 ];
+	pool.query(command, values).then(data => {
+		console.log(data);
+		templateVars.addresses = data.rows;
+		res.render('cats', templateVars);
+	})
+})
+
+
+
+
+
+
+
+
 app.get('/', (req, res) => {
 	res.render('home');
 })
 // app.get('/sandbox/:id')
 
 app.get('/sandbox', (req, res) => {
+	console.log('req.params', req.params);
+	console.log('req.body', req.body);
+	console.log('req.query', req.query);
 	const templateVars = {};
 	if (!req.query.command) {
 		return res.render('sandbox', templateVars);
 	}
-	console.log('req.query ===>', req.query);
+
 	pool.query(req.query.command).then(data => {
 		templateVars.result = data.rows;
 		res.render('sandbox', templateVars);
-	}).catch(e => {
-		templateVars.error = e.message;
+	}).catch(err => {
+		templateVars.error = err.message;
 		res.render('sandbox', templateVars);
 	})
 })
 
 app.get('/employees/:id', (req, res) => {
 	const templateVars = {};
-	console.log(req.params);
-	// the Right way of doing it!!!
-	const command = `
-	SELECT first_name, last_name, street_name
-	FROM employees
-	JOIN addresses ON employees.id = addresses.employee_id
-	WHERE employees.id = $1;`
-	pool.query(command, [req.params.id]).then( data => {
+	console.log('req.params', req.params);
+	console.log('req.body', req.body);
+	console.log('req.query', req.query);
+	// SELECT * FROM employees where employees.id = 3;
+	// const values = [];
+	// values.push(req.params.id);
+
+
+	pool.query('SELECT * FROM employees where employees.id = $1', [req.params.id]).then(data => {
 		templateVars.employee = data.rows;
 		res.render('employees_index', templateVars);
-	}).catch( e => {
-		console.log("ERROR");
-		console.log(e);
-		res.send(`Error: ${e.message}`);
+	}).catch(e => {
+		console.log(e.message);
+		res.send("Invalid parameters");
 	})
+
 })
 
 app.get('/departments', (req, res) => {
 	const templateVars = {};
-	const command = "SELECT * FROM departments;";
-	pool.query(command).then(data => {
-		console.log(data);
-		templateVars.result = data.rows;
-		templateVars.fields = data.fields;
+	const command = "SELECT * FROM departments WHERE id = $1;";
+	const values = [ 2 ];
+	pool.query(command, values).then(data => {
+		templateVars.rows = data.rows;
 		res.render('departments_index', templateVars);
-	}).catch(e => {
-		res.send(`Error: ${e.message}`);
 	})
+
 })
 
 app.get('/api/departments', (req, res) => {
 	const command = "SELECT * FROM departments;";
+
 	pool.query(command).then(data => {
 		res.json(data.rows);
-	}).catch(e => {
-		res.send(`Error: ${e.message}`);
 	})
 })
 
-app.listen(PORT, ()=> console.log("Server is listening on:", PORT));
+app.listen(PORT, () => console.log("Server is listening on:", PORT));
