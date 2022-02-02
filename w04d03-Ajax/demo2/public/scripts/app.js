fa58e0810de5f50b839a6e5efbf47f119011f2c6
vasiliy-klimkin <vasily.klimkin@gmail.com>
2022-02-02 12:40:52 -0500

    :books: live lecture added

--- w04d03-Ajax/demo2/public/scripts/app.js
+++ w04d03-Ajax/demo2/public/scripts/app.js
@@ -1,141 +1,199 @@
-// I need to figure out a way to send a request to the API
-  // JQUERY
-  // GET  /tweets
-  // POST /tweets
-const dummyShowObject = {
-    "score": 0.6971376,
-    "show": {
-        "id": 44579,
-        "url": "https://www.tvmaze.com/shows/44579/top-dog",
-        "name": "Top Dog",
-        "type": "Scripted",
-        "language": "Swedish",
-        "genres": [
-            "Drama",
-            "Crime",
-            "Thriller"
-        ],
-        "status": "To Be Determined",
-        "runtime": 45,
-        "averageRuntime": 45,
-        "premiered": "2020-10-07",
-        "ended": null,
-        "officialSite": "https://www.cmore.se/serie/148290-top-dog",
-        "schedule": {
-            "time": "21:00",
-            "days": ["Wednesday"]
-        },
-        "rating": {
-            "average": null
-        },
-        "weight": 44,
-        "network": {
-        "id": 1367,
-        "name": "C More",
-        "country": {
-            "name": "Sweden",
-            "code": "SE",
-            "timezone": "Europe/Stockholm"
-        }
-        },
-        "webChannel": {
-            "id": 170,
-            "name": "C More",
-            "country": {
-                "name": "Sweden",
-                "code": "SE",
-                "timezone": "Europe/Stockholm"
-            }
-        },
-        "dvdCountry": null,
-        "externals": {
-            "tvrage": null,
-            "thetvdb": 377684,
-            "imdb": "tt11542694"
-        },
-        "image": {
-        "medium": "https://static.tvmaze.com/uploads/images/medium_portrait/276/691903.jpg",
-        "original": "https://static.tvmaze.com/uploads/images/original_untouched/276/691903.jpg"
-        },
-        "summary": "<p><b>Top Dog</b> centres on the clash between ambitious business lawyer Emily Jansson and the ex-crook Teddy Maksumic. A mysterious disappearance brings together two completely different worlds, and an extremely ill-matched couple - lawyer Emily and the conditionally released Teddy. The two are at loggerheads. She wants up. He wants out. She wants to secure her future; he wants to leave his old life behind. The pair are forced to rethink when they end up in a tricky situation where the past catches up, where future dreams are shattered, and where nothing is what it seems.</p>",
-        "updated": 1606764898,
-        "_links": {
-            "self": {
-            "href": "https://api.tvmaze.com/shows/44579"
-            },
-            "previousepisode": {
-                "href": "https://api.tvmaze.com/episodes/1956412"
-            }
-        }
-    }
-}
+// Check if the script file is correctly referenced!!
+console.log("do I work!?");
 
-const defaultImg = "https://i.pinimg.com/originals/1d/e8/b3/1de8b334143659ea628a7ee620d2f6ab.jpg"
+//  Steps in building the application
 
-const addMovie = (movieObj) => {
-    let img = defaultImg;
 
-    if (movieObj.show.image) {
-        img = movieObj.show.image.medium;
-    }
-
-    $('.movies').append(`
-    <div class="movie">
-        <img class="movie--img" src="${img}"/>
-        <div class="movie--desc">
-            <h1>${movieObj.show.name}</h1>
-            <h2>${movieObj.score}</h2>
-            <h3>${movieObj.show.genres}</h3>
-            <h4>${movieObj.show.language}</h4>
-            <h5>${movieObj.show.premiered}</h5>
-        </div>
-        <p class="movie--summary">
-        ${movieObj.show.summary}
-        </p>
-    </div>
-    `);
-}
-
-// we wannna see
-// rating
-// title
-// genres
-// image
-// country
-// language
-// premiered
-
-// form ----> event listner ------->  preventDefault
-
-// evt.target.movieName
-/////////////////////////////////////////////////////////////////////
-// serialize the value 
-// const val = $(evt.target.movieName).serialize();
-//
-// $.post('/tweets/', val).then(() => {
-
+// We are going to check if we can deal with AJAX request (can we get data ?)
+// get my API URL
+const apiURL = "https://api.tvmaze.com/search/shows?q=";
+// try to get the data out using /fetch/jquery ajax
+//  WHATEVER you GOOGLE for JQUERY will DIFFER from your classmates
+//////////////////////////////////////////////////////////
+// $.get(apiURL).then((data) => {
+//     console.log("did this work!? 2222");
+//     console.log(data);
 // })
-
-////////////////////////////////////////////////////////////////////
-
-$(() => {
-    let tvMazeApiURL = 'https://api.tvmaze.com/search/shows?q=';
-    /// EVENT LISTNER FOR FORM TO CONTROL THE SEARCH OPTION
-    // add this inside of document.on.ready because just like append()
-    // you can only attach an event listener to a loaded HTML tag
-    $('#search').on('submit', (evt) => {
-        evt.preventDefault();
-        const val = $(evt.target.movieName).serialize();
-        console.log("VAL--->", val);
-        console.log(evt.target.movieName.value);
-        $('.movies').empty();
-        $.get(tvMazeApiURL + evt.target.movieName.value).then(data => {
-            console.log("what happened?");
-            console.log(data);
-            for (movie of data) {
-              addMovie(movie);
+//////////////////////////////////////////////////////////
+// MOCK the information that we got from the server ()
+//  some api requests COST MONEY
+const mockData = [
+    {
+        "score": 0.6892167,
+        "show": {
+            "id": 58020,
+            "url": "https://www.tvmaze.com/shows/58020/scaredy-cats",
+            "name": "Scaredy Cats",
+            "type": "Scripted",
+            "language": "English",
+            "genres": ["Comedy", "Family", "Fantasy"],
+            "status": "To Be Determined",
+            "runtime": null,
+            "averageRuntime": 28,
+            "premiered": "2021-10-01",
+            "ended": null,
+            "officialSite": "https://www.netflix.com/title/81329721",
+            "schedule": {
+                "time": "",
+                "days": []
+            },
+            "rating": {
+                "average": null
+            },
+            "weight": 79,
+            "network": null,
+            "webChannel": {
+                "id": 1,
+                "name": "Netflix",
+                "country": null
+            },
+            "dvdCountry": null,
+            "externals": {
+                "tvrage": null,
+                "thetvdb": null,
+                "imdb": null
+            },
+            "image": {
+                "medium": "https://static.tvmaze.com/uploads/images/medium_portrait/362/906042.jpg",
+                "original": "https://static.tvmaze.com/uploads/images/original_untouched/362/906042.jpg"
+            },
+            "summary": "<p>A beautiful surprise leads to a paws-itively peculiar adventure for Willa, Lucy, and Scout. Can they handle the mischief - and outwit a talking rat?</p>",
+            "updated": 1633267338,
+            "_links": {
+                "self": {
+                    "href": "https://api.tvmaze.com/shows/58020"
+                },
+                "previousepisode": {
+                    "href": "https://api.tvmaze.com/episodes/2184562"
+                }
             }
-          })
+        }
+    },
+    {
+        "score": 0.6892167,
+        "show": {
+            "id": 58020,
+            "url": "https://www.tvmaze.com/shows/58020/scaredy-cats",
+            "name": "Scaredy Cats",
+            "type": "Scripted",
+            "language": "English",
+            "genres": ["Comedy", "Family", "Fantasy"],
+            "status": "To Be Determined",
+            "runtime": null,
+            "averageRuntime": 28,
+            "premiered": "2021-10-01",
+            "ended": null,
+            "officialSite": "https://www.netflix.com/title/81329721",
+            "schedule": {
+                "time": "",
+                "days": []
+            },
+            "rating": {
+                "average": null
+            },
+            "weight": 79,
+            "network": null,
+            "webChannel": {
+                "id": 1,
+                "name": "Netflix",
+                "country": null
+            },
+            "dvdCountry": null,
+            "externals": {
+                "tvrage": null,
+                "thetvdb": null,
+                "imdb": null
+            },
+            "image": {
+                "medium": "https://static.tvmaze.com/uploads/images/medium_portrait/362/906042.jpg",
+                "original": "https://static.tvmaze.com/uploads/images/original_untouched/362/906042.jpg"
+            },
+            "summary": "<p>A beautiful surprise leads to a paws-itively peculiar adventure for Willa, Lucy, and Scout. Can they handle the mischief - and outwit a talking rat?</p>",
+            "updated": 1633267338,
+            "_links": {
+                "self": {
+                    "href": "https://api.tvmaze.com/shows/58020"
+                },
+                "previousepisode": {
+                    "href": "https://api.tvmaze.com/episodes/2184562"
+                }
+            }
+        }
+    },
+
+]
+//  if we over do our requests, we will have to pay for it...
+// DISPLAY the mock information on our web page
+//   |---->>> Layout
+
+
+const appendMovie = (movie) => {
+    console.log(movie);
+    const defaultIMG = 'https://i.kym-cdn.com/photos/images/newsfeed/001/394/314/c62.jpg';
+    const layout = `
+    <div class="movie">
+    <img 
+        class="movie--img"
+        src="${movie.show.image.medium}" 
+    />
+    <div class="movie--info">
+        <h2>${movie.show.name}</h2>
+        <h3>${movie.show.genres}</h3>
+        <h3>${movie.show.premiered}</h3>
+        <p>${movie.show.summary}</p>
+    </div>
+    </div>
+`;
+    $('#movies').append(layout);
+}
+
+const appendMulitpleMovies = (movieArr) => {
+    for (let movie of movieArr) {
+        appendMovie(movie);
+    }
+}
+
+// DOCUMENT ON READY SAME THING
+$(() => {
+    // REPLACE the MOCK information on our web page
+    // $.get(apiURL).then((data) => {
+    //     console.log("did this work!? 2222");
+    //     console.log(data);
+    //     appendMulitpleMovies(data);
+    // })
+    // when adding an event listener, ALWAYS PUT IN IN THE DOC.on.ready
+    $('#movie--search').on('submit', (evt) => {
+        evt.preventDefault();
+        $('#movies').empty();
+        const name = evt.target.searchParam.value;
+        // POST METHOD
+        ////////////////////////////////////////////
+        const param = $('#movie--search').serialize();
+        $.post('/tweets', param).then(() => {
+
+        })
+
+
+
+
+
+        //////////////////////////////////////////////////
+        // /tweets POST <---- SAVE A TWEET to the "database"
+        //  GET METHOD
+
+        console.log(name);
+        const searchURL = apiURL + name;
+        console.log(searchURL);
+        $.get(searchURL).then((data) => {
+            console.log(data);
+            appendMulitpleMovies();
+        })
+        console.log(param);
+        // get the text from the input field..
+        // append it to my API URL
+        // get the new data 
+        // and append the data to the website
+        console.log("HELLO WORLD!");
     })
 });
 
@@ -143,21 +201,4 @@ $(() => {
 
 
 
-
-
-
-// HOW do I, add an item FROM app.js to an index.html file ?
-
-  let tvMazeApiURL = 'https://api.tvmaze.com/search/shows?q=dog';
-//   $.get(tvMazeApiURL).then(data => {
-//       console.log("what happened?");
-//       console.log(data);
-//   })
-
-
-// I need to see what sort of data object/array im working with
-
-// I need to build a layout for these data object(s)
-
-// Styling :) 
-
+// a better "dynamic" way of building this web page
