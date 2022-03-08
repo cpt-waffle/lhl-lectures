f479984fc541b57781bc5602174a86d0989c3cfe
vasiliy-klimkin <vasily.klimkin@gmail.com>
2022-03-08 10:15:23 -0500

    :books: setups

--- w01d02-The-Dev-Workflow/w01d02-demo/demo.js
+++
@@ -1,8 +0,0 @@
-// git init                    --- initialized the repo
-// git add file_name           --- add to staging
-// git add .                   --- add all files in THIS folder into staging
-// git commit -m "__message__" --- stage the file(s) with the proper commit message!
-// git push origin main        --- push the commits ( and files ) to github main branch
-// git push origin __branch__name__
-console.log("Hello World!");
-console.log("another hello world!");
