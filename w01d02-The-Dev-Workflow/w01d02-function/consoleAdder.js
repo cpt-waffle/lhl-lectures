9363973be3cf7e0ca0d39b950625c990b338edc1
vasiliy-klimkin <vasily.klimkin@gmail.com>
2022-03-08 10:16:31 -0500

    :books: setups

--- w01d02-The-Dev-Workflow/w01d02-function/consoleAdder.js
+++
@@ -1,4 +0,0 @@
-// We are tasked to create a program that takes 
-// in any number of command line arguments
-// which will be numbers, and adds them toghether 
-// in and returns a sum of all the numbers.
