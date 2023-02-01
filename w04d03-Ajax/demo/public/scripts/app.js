abfb679de1fbbf2f0a95e7f6393fa9b7f1684e93
vasiliy-klimkin <vasily.klimkin@gmail.com>
2023-02-01 13:01:04 -0500

    :books: live lecture added

--- w04d03-Ajax/demo/public/scripts/app.js
+++ w04d03-Ajax/demo/public/scripts/app.js
@@ -1,65 +1,75 @@
-console.log("hello world");
+
 
 const PI = 3.14;
+
 const hello = () => {
-  console.log("Hello from APP.js LINE 5.")
+
 }
 
-// How the heck am i going to get data from our TV MAZE API to our website!!
-const getShows = () => {
-  $.get('https://api.tvmaze.com/search/shows?q=cars', (data) => {
-    console.log("data", data);
-  })
+const appendMovies = (movieArr) => {
+  // gets an array of movies/shows
+  // loops through them and appends each movie to our HTML page
+  for (movieObj of movieArr) {
+    appendMovie(movieObj);
+  }
 }
 
+const appendMovie = (movie) => {
+  const image = 'https://i.kym-cdn.com/photos/images/newsfeed/001/394/314/c62.jpg';
+  
+  let img = image;
 
-const appendMovie = movie => {
-  const defaultPic = "https://static.wikia.nocookie.net/1f6531b7-9d8b-4015-b0e5-a789ba30bd58";
-  console.log(movie.show.name);
-  const movieTemplate = `
-  <div class="movie">
-  <img  class="movie--img" src="${movie.show.image.original}"/>
-  <div class="movie--info">
+  if (movie.show.image) {
+    img = movie.show.image.original;
+  }
+  
+  const movieHTML = `
+    <article class="movie-container">
+    <img 
+      class="movie-container-image"
+      src="${img}"
+    />
     <h2>${movie.show.name}</h2>
-    <h3>Rating: ${movie.score}</h3>
-    ${movie.show.summary}
-  </div>
-  </div>
-`;
-
-  $( "#movies" ).append(movieTemplate);
+    <h2>${movie.show.rating.average ? movie.show.rating.average : 'no yet rated' }</h2>
+    <h3>${movie.show.premiered}</h3>
+    <p>
+      ${movie.show.summary}
+    </p>
+  </article>
+  `
+  $('#movies').append(movieHTML);
 }
 
-const appendMovies = moviesArr => {
-  // for (let movie of moviesArr) {
-  //   appendMovie(movie);
-  // }
 
-  moviesArr.forEach((movie) => {
-    appendMovie(movie);
-  })
+const getMovies = (str) => {
+  $.get(`https://api.tvmaze.com/search/shows?q=${str}`, function(data) {
+    $('#movies').empty();
+    appendMovies(data);
+  });
 }
 
-// GET /TWEETS
-// POST /TWEETS
-// remove all the tweets from your HTML page
-// and re-add em by using GET /TWEETS
+
+// things to put into the document.on.ready
+// functions that change DOM elements ( so anyhting on the HTML document)
+// event listners
+
+// $('#search').on('hover')
 
 $(() => {
-  $('#search-form').on('submit', (evt) => {
+  getMovies('cars');
+  $('#search').on('submit', (evt) => {
     evt.preventDefault();
-    $.get(`https://api.tvmaze.com/search/shows?q=${evt.target.search.value}`, (data) => {
-      console.log("data", data);
-      $('#movies').empty();
-      appendMovies(data);
-    })
-  })
+    const str = $('#search-input').val();
+    getMovies(str);
+  });
 })
 
 
-// We got information, how do we 
-
-// create a layout to display this information?
-// how do we append this information to the page?
-// how do load more of this information to this page?
-// how do we make it more user interactive?
+/// 1) figure out a layout of how our movies are going to look like on the page
+//      build this in HTML/css
+//  1.5) figure out how to append our Movie container programmatically
+//  2) figure out how to append data
+//  3) figure out how to do that on load
+//  3.5)  from the ajax call to our HTML page
+//  4) we want to search the movies ourselves, need figure out how to add a form
+// and make custom search string parameters
