52f64190f33dc42d6884e2a15f4e1937160c12b7
vasiliy-klimkin <vasily.klimkin@gmail.com>
2023-04-11 12:35:56 -0400

    :books: live lecture added

--- w10d03-Advanced-WebSockets/demo/client/src/App.js
+++ w10d03-Advanced-WebSockets/demo/client/src/App.js
@@ -1,34 +1,29 @@
-import {io} from 'socket.io-client';
 import './App.css';
-import {useEffect, useState} from 'react';
-import 'react-toastify/dist/ReactToastify.css';
+// socket.io-client
+import {io} from 'socket.io-client';
+import {useState, useEffect} from 'react';
 import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const socket = io();
 
 
 function App() {
-
   const [name, setName] = useState('');
   const [users, setUsers] = useState([]);
   const [messages, setMessages] = useState([]);
 
   useEffect(() => {
-    socket.on('connect', () => {
-      console.log("connected!");
-    })
-
     socket.on('INITIAL_CONNECTION', payload => {
-      console.log('initial connection!');
-      console.log(payload);
+      console.log("payload has come in!", payload);
       setName(payload.name);
       setUsers(payload.users);
     })
 
     socket.on('NEW_USER', payload => {
-      console.log("new user!");
+      console.log("new user has connected!");
       console.log(payload);
-      toast.success(`User ${payload.name} has connected!`, {
+      toast.success(`New User! ${payload.name} has connected :)  !!!`, {
         position: "top-center",
         autoClose: 1000,
         hideProgressBar: false,
@@ -38,64 +33,47 @@ function App() {
         progress: undefined,
         theme: "colored",
         });
-      setUsers(prev => [...prev, payload.name])
+      setUsers(prev => [...prev, payload.name]);
     })
 
-    socket.on("SEND_MSG", payload => {
-      console.log(payload);
-      setMessages(prev => [...prev, payload])
+    socket.on('SEND_MSG', payload => {
+      console.log("payload", payload);
+      setMessages(prev => [...prev, payload.msg]);
     })
 
-    
-    socket.on('DISCONNECT', payload => {
-      console.log('disconnected!');
-      console.log(payload);
-      toast.error(`User ${payload.name} has disconnected!`, {
-        position: "top-center",
-        autoClose: 1000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "colored",
-        });
-      setUsers(prev => prev.filter(name => name !== payload.name));
-    })
 
     return () => {
-      //
-      console.log("unmounting.....");
-      socket.off('connect');
       socket.off('INITIAL_CONNECTION');
       socket.off('NEW_USER');
       socket.off('SEND_MSG');
-      socket.off('DISCONNECT');
-    }    
-  }, []);
+    }
 
-  const sendMessage = evt => {
+  }, [])
+
+  const onMessageSubmit = (evt) => {
     evt.preventDefault();
-    console.log(evt.target.msg.value);
-    socket.emit('SEND_MSG', {name, msg: evt.target.msg.value});
+    const msg = evt.target.message.value;
+    console.log("msg", msg);
+    socket.emit('SEND_MSG', { msg: `${name}: ${msg}` });
   }
-
 
   return (
     <div className="App">
-      <h1>Chat App :)</h1>
-      {name ? <h2>Current User: {name}</h2> : <h2>Getting Name...</h2>}
-      {users.map(user => <li>{user}</li>)}
-
+      <h1>Chat App!</h1>
+      <h3> You are : {name} </h3>
+      <ul>
+        {users.map(user => <li>{user}</li>)}
+      </ul>
 
       <ul>
-        {messages.map(msg => <li>{msg.name}: {msg.msg}</li>)}
+        {messages.map(msg => <li>{msg}</li>)}
       </ul>
-      <form onSubmit={sendMessage}>
-        <input type="text" name="msg"/>
-        <button>Send Message!</button>
+      <form onSubmit={onMessageSubmit}>
+        <input type="text" name="message"/>
+        <button>Send!</button>
       </form>
-      <ToastContainer/>
+
+      <ToastContainer />
     </div>
   );
 }
--- w10d03-Advanced-WebSockets/demo/server/server.js
+++ w10d03-Advanced-WebSockets/demo/server/server.js
@@ -1,43 +1,39 @@
 const PORT = 8080;
+
 const express = require('express');
+const morgan  = require('morgan');
+const http    = require('http');
 const { Server } = require('socket.io');
-const http = require('http');
 const ikea = require('ikea-name-generator');
 
 const app = express();
 const server = http.createServer(app);
+
+// initializing socket.io server
 const io = new Server(server);
+
 const users = [];
 
-//                  a connection between A and B
 io.on('connection', (socket) => {
-  console.log('Hello There?');
-  const name = ikea.getName();
-  socket.name = name;
+  // 2 parameters
+  // message type, payload
+  const name = ikea.getName()
   users.push(name);
-  //           msg type              payload
-  socket.emit('INITIAL_CONNECTION', { name, users });
+  socket.name = name;
+  socket.emit('INITIAL_CONNECTION', {name, users});
   socket.broadcast.emit('NEW_USER', { name });
-
   socket.on('SEND_MSG', payload => {
-    console.log("send msg!");
-    console.log(payload);
+    console.log("SEND_MSG", payload);
     io.emit('SEND_MSG', payload);
   })
-
-
-  socket.on("disconnect", (reason) => {
-    console.log("name", socket.name);
-    console.log(":( disconnect");
-    socket.broadcast.emit('DISCONNECT', { name: socket.name });
-  });
 })
+/////////////////////////////////
 
-/////////////////////////////////////////////////////////
+app.use(morgan('dev'));
+
 
 app.get('/', (req, res) => {
-  res.json({foo: 'bar'});
+  return res.json({foo: 'bar'});
 });
 
-
-server.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server is listening on PORT ${PORT}`));
