60117d8801955165f99312e81ca253a3cdccc1a6
vasiliy-klimkin <vasily.klimkin@gmail.com>
2023-04-19 12:56:08 -0400

    :books: live lecture added

--- w07d2-Immutable-Patterns/demo/src/App.js
+++ w07d2-Immutable-Patterns/demo/src/App.js
@@ -1,108 +1,133 @@
-import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react'; 
-
+import FancyButton from './components/FancyButton';
+import { useState } from 'react';
 
 // whats state in react?
 // how is it different from a let/const ?
+
+let id = 5;
 function App() {
-  let number = 0;
-  // react useState -- needs to return 2 things
-  //  first element of the array represents the VALUE of the state
-  //  second element of the array represents the FUNCTION to CHANGE THAT VALUE
-  const [num, changeNum] = useState(22);
-  const [inputTask, setInputTask] = useState('');
+  console.log("LINE 7, we are running App();");
+  let num = 0;
+  // first element of this array, is your STATE VALUE
+  // second element is the function to CHANGE that state
+  const [number, setNumber] = useState(1);
+  const [number2, setNumber2] = useState(2);
+  const [newTask, setNewTask] = useState('');
+
   const [tasks, setTasks] = useState([
     {id: 1, task: ' have a meeting '},
     {id: 2, task: ' check on finals'},
     {id: 3, task: ' go to physio'},
     {id: 4, task: ' make a sandwitch'}
   ])
-  //  ^ DO NOT EVER CHANGE STATE DIRECTLY
-  //  num = ?? X WRONG
-  console.log("re-render!");
 
-  // $('#btn').on('click', () => {
-    //...
+  // const [state, setState] = useState({ number: 1, number2: 2})
+  // const number = whatAreYou[0];
+  // const setNumber = whatAreYou[1];
+
+  // State is a variable
+  // whenever state changes, the app will re-render ( in a nice, performant way...)
+
+  // $(() => {
+    // $('#btn').on('click', () => {
+    //....
+    // num++;
+    // })
+
   // })
-  // Re-render --- can occur when a state changes
-  // when you change a variable, the re-render of the component does not occur
 
-  const onButtonClick = () => {
-    console.log("yay!!");
-    // when we use a state changing function,
-    // react does the change, but also attempts to re-render the component
-    // if the value was changed, the re-render will occur
-    // changeNum(num + 1); //  22 + 1 = 23
-    //           ^--- Immutability 
-    // Batching...
-    //         prev -- previous state
-    // changeNum((prev) => {
-    //   return prev + 1;
-    // });
-    //        ^-- values, an array and number, etc
-    //        ^-- it can take a callback...
-    const task = {task: 'make sure to do immutable patterns!'};
-    // spread operator...
-    // ...
-    //  ^--- copy (shallow copy) the contents of _____
-    //  [1,2,3,4,5]
-    // const tasksCopy = [ task, ...tasks];
-    // for (let item of tasks) {
-    //   tasksCopy.push(item);
-    // }
-    // tasksCopy.push(task)
-
-    setTasks(prev => {
-      return [...prev, task]
+// Rendering cycle
+// state
+// VirtualDOM
+  // 2 or 12
+  const onClick = () => {
+    console.log("something has been clicked!");
+    // batching 
+    setNumber(prev => {
+      return prev + 1;
+    }); // async
+    setNumber(prev => {
+      return prev + 1;
+    });
+    setNumber(prev => {
+      return prev + 1;
+    });
+    setNumber(prev => {
+      return prev + 1;
+    });
+    setNumber(prev => {
+      return prev + 1;
+    });
+    setNumber(prev => {
+      return prev + 1;
     });
 
-
+    //1) changes the state
+    //2) tells the component you are in to re-render
   }
 
-  const array = [<h1>Hello world</h1>, <h2>Test</h2>, <p>this is a p tag</p>];
-  const obj = {a: 1}
-  // Virtual DOM
-  // {
-    // h1__1: {typeof$$: symbol.react, value: 'React W7D3 - Immutable State' type: 'h1 }
-    // h1__2: {typeof$$: symbol.react, value: 'React W7D3 - Immutable State' type: 'h1 }
-    // h2__3: {.......  value: {
-      //  h1: { }
-    // } }
-    // ??: {a: 1}
-    // li_1_34252: {3}
-    // li_2_34253: {1}
-    // li_3_34354: {2}
+  const onClick2 = () => {
+    setNumber2(number2 + 2);
+  }
+
+  const a = <h1>hello world</h1>;
+  console.log(a);
+
+  // VirtualDOM
+  //  {
+        // ...
+        // h2-1: {type: 'h2', children: {text: 'Number', number: 2}}
+        // li-0:  {type: 'li', children: {text: 'Number', number: 2}}
+        // li-1: {type: 'li', children: {text: 'Number', number: 2}}
+        // li-2: {type: 'li', children: {text: 'Number', number: 2}}
+
+
+        // ...
   // }
-  
-  // try not to use this way, its not the way we like to use it in react...
-  const tasksHTML = [];
 
-  for (let item of tasks) {
-    tasksHTML.push(<li>{item.task}</li>)
-  }
-  //////////////////////////////////////////////////////////
+  // tasks.map( (task) => {
+    // return <li>task</li>
+  // })
 
-  const tasksHTML2 = tasks.map(item => <li key={item.id}>{item.task}</li>)
+  // const tasksHTMLArray = [];
+  // for (let task of tasks) {
+    // tasksHTMLArray.push(<li>{task}</li>)
+  // }
 
-  const onFormSubmit = evt => {
-    console.log("on form submit!");
+
+  const onTaskSubmission = evt => {
     evt.preventDefault();
-    setTasks(prev => [...prev, {task: inputTask}])
+    console.log(newTask); // we need to add to task state
+    // tasks.push({id: 6, task: newTask});
+    // ... <--- SHALLOW COPY
+    // setTasks([ ...tasks, {id: id, task: newTask}]);
+    setTasks(prev => {
+      return [...prev, {id: id, task: newTask}]
+    })
+    id++;
   }
 
 
   return (
     <div className="App">
       <h1>React W7D3 - Immutable State Patterns</h1>
-      {/*EJS <= number %>  ObjectObject*/}
-      <h2>Number {num}</h2>
-      {tasksHTML2}
-      <form onSubmit={onFormSubmit}>
-        <input type='text' name='newtask' value={inputTask} onChange={(evt) => setInputTask(evt.target.value)}/>
+      <h2>Number { number }</h2>
+      <button onClick={onClick} id="btn">Click me</button>
+      <h2>Number2 { number2 }</h2>
+      <button onClick={() => setNumber2(number2 + 2)} id="btn">Click me for2</button>
+
+      <ul>
+        {tasks.map(task => <li key={task.id}>{task.task}</li>)}
+      </ul>
+
+      <form onSubmit={onTaskSubmission}>
+        <input type="text" name="new_task" value={newTask} onChange={(evt) => setNewTask(evt.target.value)}/>
         <button>Add Task</button>
       </form>
-      <button onClick={onButtonClick}>Click Me</button>
+
+
+
     </div>
   );
 }
---
+++ w07d2-Immutable-Patterns/demo/src/components/FancyButton.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+
+const FancyButton = (props) => {
+  return (<button onClick={props.onClick} className="fancy">FANCY BUTTON</button>)
+}
+
+
+export default FancyButton;
