9953ec67ba0c7ec245ff5f7471748daa0ac95fc1
vasiliy-klimkin <vasily.klimkin@gmail.com>
2023-05-09 13:01:13 -0400

    :books: live lecture added

--- w10d03-Advanced-WebSockets/demo/client/src/App.js
+++ w10d03-Advanced-WebSockets/demo/client/src/App.js
@@ -1,31 +1,32 @@
 import './App.css';
-// socket.io-client
+// to connect to the backend server
 import {io} from 'socket.io-client';
-import {useState, useEffect} from 'react';
+import { useEffect, useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const socket = io();
 
-
 function App() {
-  const [name, setName] = useState('');
+
+  const [user, setUser] = useState('');
   const [users, setUsers] = useState([]);
   const [messages, setMessages] = useState([]);
 
   useEffect(() => {
-    socket.on('INITIAL_CONNECTION', payload => {
-      console.log("payload has come in!", payload);
-      setName(payload.name);
-      setUsers(payload.users);
+    socket.on('INITIAL_CONNECTION', (payload) => {
+      console.log("Data has come in!!");
+      console.log(payload);
+      setUser(payload.name);
+      setUsers([...payload.users]);
     })
 
     socket.on('NEW_USER', payload => {
-      console.log("new user has connected!");
+      console.log("NEW USER!!");
       console.log(payload);
-      toast.success(`New User! ${payload.name} has connected :)  !!!`, {
+      toast.info(`New User has connected ${payload.name}`, {
         position: "top-center",
-        autoClose: 1000,
+        autoClose: 500,
         hideProgressBar: false,
         closeOnClick: true,
         pauseOnHover: true,
@@ -37,36 +38,32 @@ function App() {
     })
 
     socket.on('SEND_MSG', payload => {
-      console.log("payload", payload);
-      setMessages(prev => [...prev, payload.msg]);
+      console.log("message has come in!");
+      console.log(payload);
+      setMessages(prev => [...prev, payload]);
     })
-
 
     return () => {
       socket.off('INITIAL_CONNECTION');
       socket.off('NEW_USER');
       socket.off('SEND_MSG');
     }
-
-  }, [])
+  }, []);
 
   const onMessageSubmit = (evt) => {
     evt.preventDefault();
-    const msg = evt.target.message.value;
-    console.log("msg", msg);
-    socket.emit('SEND_MSG', { msg: `${name}: ${msg}` });
+    socket.emit('SEND_MSG', {msg: evt.target.message.value})
   }
 
   return (
     <div className="App">
       <h1>Chat App!</h1>
-      <h3> You are : {name} </h3>
+      <h3>You are:  {user ? user : <span>Loading...</span>}</h3>
       <ul>
         {users.map(user => <li>{user}</li>)}
       </ul>
-
       <ul>
-        {messages.map(msg => <li>{msg}</li>)}
+        {messages.map(message => <li>{message.msg}</li>)}
       </ul>
       <form onSubmit={onMessageSubmit}>
         <input type="text" name="message"/>
--- w10d03-Advanced-WebSockets/demo/server/server.js
+++ w10d03-Advanced-WebSockets/demo/server/server.js
@@ -1,39 +1,49 @@
 const PORT = 8080;
 
 const express = require('express');
+//3 require socket.io
+const { Server }= require('socket.io');
+//1  add http module
+const http = require('http');
 const morgan  = require('morgan');
-const http    = require('http');
-const { Server } = require('socket.io');
+
 const ikea = require('ikea-name-generator');
 
 const app = express();
+//2 wrap app in http.createServer
 const server = http.createServer(app);
-
-// initializing socket.io server
+//4 wrap SocketIo server with http server
 const io = new Server(server);
-
-const users = [];
-
-io.on('connection', (socket) => {
-  // 2 parameters
-  // message type, payload
-  const name = ikea.getName()
-  users.push(name);
-  socket.name = name;
-  socket.emit('INITIAL_CONNECTION', {name, users});
-  socket.broadcast.emit('NEW_USER', { name });
-  socket.on('SEND_MSG', payload => {
-    console.log("SEND_MSG", payload);
-    io.emit('SEND_MSG', payload);
-  })
-})
-/////////////////////////////////
 
 app.use(morgan('dev'));
 
+// ws + wss
+// socket.io
 
-app.get('/', (req, res) => {
-  return res.json({foo: 'bar'});
-});
+app.get('/', (req,res) => {
+  return res.json({hello: 'world!'});
+})
 
-server.listen(PORT, () => console.log(`Server is listening on PORT ${PORT}`));
+/////////////////// IO LISTENERS //////////////////////
+const users = [];
+
+io.on('connection', socket => {
+  console.log("Someone has Connected!!!");
+  const name = ikea.getName(false);
+  users.push(name);
+  socket.emit('INITIAL_CONNECTION', {name, users});
+  socket.broadcast.emit('NEW_USER', {name});
+
+  socket.on('SEND_MSG', payload => {
+    console.log("Message recieved from client!");
+    console.log(payload);
+    io.emit('SEND_MSG', payload);
+  })
+})
+
+
+
+
+
+//5  server.listen 
+server.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
