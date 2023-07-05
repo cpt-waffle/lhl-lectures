f1fbccd7897d944cccee5399f8f8dc7e887982bf
vasiliy-klimkin <vasily.klimkin@gmail.com>
2023-07-05 13:14:54 -0400

    :books: live lecture added

--- w10d03-Advanced-WebSockets/demo/client/src/App.js
+++ w10d03-Advanced-WebSockets/demo/client/src/App.js
@@ -1,87 +1,82 @@
 import './App.css';
 import {io} from 'socket.io-client';
-import {useEffect, useState} from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import {useEffect, useState} from 'react';
 
 const socket = io();
 
 function App() {
-  const [user, setUser] = useState('');
+  const [name,setName] = useState('');
   const [users, setUsers] = useState([]);
   const [messages, setMessages] = useState([]);
 
-  useEffect(() => {           // from server { user }
-    socket.on("INITIAL_CONN", payload => {
-      console.log("payload");
+  useEffect(() => {
+    console.log("use-effect!");
+    socket.on('INITIAL_CONN', (payload) => {
+      console.log("intial_conn");
       console.log(payload);
-      setUser(payload.user);
+      setName(payload.name);
       setUsers(payload.users);
     })
-    
-    socket.on('NEW_USER', payload => {
-      console.log("new user--------");
+
+    socket.on("NEW_USER", (payload) => {
+      console.log("new_user");
       console.log(payload);
-      // prev =>
-      console.log('user', user); // user = ''
-      console.log('users', users); // users = []
-      toast.info(`New User: ${payload.user}`, {
+      toast.info(`🦄New User has connected:  ${payload.name}`, {
         position: "top-center",
-        autoClose: 1000,
+        autoClose: 2000,
         hideProgressBar: false,
         closeOnClick: true,
         pauseOnHover: true,
         draggable: true,
         progress: undefined,
-        theme: "colored",
+        theme: "dark",
         });
-      setUsers(prev => {
-        console.log("PREVIOUS STATE WAS...", prev);
-        return [...prev, payload.user];
-      })
-
+      setUsers(prev => [...prev, payload.name]);
     })
 
     socket.on('SEND_MSG', payload => {
-      console.log("message came back");
+      console.log('send_msg');
       console.log(payload);
       setMessages(prev => [...prev, payload]);
     })
 
     return () => {
+      console.log("unmount!");
       socket.off('INITIAL_CONN');
       socket.off('NEW_USER');
       socket.off('SEND_MSG');
 
     }
 
-  }, [])
+  }, []); // <-- dependancy array
 
-  const onSubmit = evt => {
+  const onSubmit = (evt) => {
     evt.preventDefault();
-    console.log(evt.target.message.value);
-    const payload = {message: evt.target.message.value, user };
-    socket.emit('SEND_MSG', payload);
+    console.log(evt.target.msg.value);
+    const msg = evt.target.msg.value;
+    evt.target.msg.value = '';
+    socket.emit('SEND_MSG', {name, msg});
   }
 
 
   return (
     <div className="App">
       <h1>Chat App!</h1>
-      {user ? <h2>{user}</h2> : <h2>Loading...</h2>}
-      <h1>Current online users</h1>
-      <ul>
-        {users.map( user => <li key={user}>{user}</li>)}
-      </ul>
-
-      <ul>
-        {messages.map( message => <li>{message.user}: {message.message}</li>)}
-      </ul>
-      <form onSubmit={onSubmit}>
-        <input type="text" name="message"/>
-        <button>Send!</button>
-      </form>
-
+      {name ? <h4>Connected as {name}</h4> : <h4>connecting...</h4>}
+      <div className='chat'>
+        <ul>
+          {users.map(user => <li>{user}</li>)}
+        </ul>
+        <form onSubmit={onSubmit}>
+          <ul>
+            {messages.map(message => <li><b>{message.name}:</b> {message.msg}</li>)}
+          </ul>
+          <input type="text" name="msg"/>
+          <button>Send!</button>
+        </form>
+      </div>
       <ToastContainer />
     </div>
   );
--- w10d03-Advanced-WebSockets/demo/server/server.js
+++ w10d03-Advanced-WebSockets/demo/server/server.js
@@ -1,31 +1,36 @@
 const PORT = 8080;
-const express = require('express');
-const morgan = require('morgan'); 
-const http = require('http');              // <-------
-const { Server } = require("socket.io");   // <-------
+//1) npm install socket.io 
 
+const express = require('express');
+const http    = require('http'); // <--
+const morgan = require('morgan');
+const {Server} = require("socket.io"); // <---
+
+///
 const ikea = require('ikea-name-generator');
 
-
-// Ws with Wss
-// Socket.io <---
-
 const app = express();
-const server = http.createServer(app);  // <-------
-const io = new Server(server);          // <-------
-/////////////////////////Socket.io///////////////////
+const server = http.createServer(app); // <---
+
+app.use(morgan('dev'));
+
+const io = new Server(server) // <--
+
+
 const users = [];
 
-io.on('connection', socket => {
-  console.log("someone has connected :)");
-  const user = ikea.getName(false);
-  //                          {user: user}
-  users.push(user);
-  socket.emit('INITIAL_CONN', { user, users });
-  socket.broadcast.emit('NEW_USER', {user});
+io.on('connection', (socket) => {  // <--
+  console.log("connected!");
+  //        event_Type,   payload {}
+  const name = ikea.getName(false);
+  users.push(name);
+  socket.emit('INITIAL_CONN', {name, users});
+  // sending a message to a connected user
 
-  socket.on('SEND_MSG', payload => {
-    console.log('send_msg');
+  socket.broadcast.emit('NEW_USER', {name});
+
+  socket.on('SEND_MSG', (payload) => {
+    console.log("SEND_MSG");
     console.log(payload);
     io.emit('SEND_MSG', payload);
   })
@@ -33,11 +38,14 @@ io.on('connection', socket => {
 })
 
 
-/////////////////////////////////////////////////////
-app.use(morgan('dev'));
+
 app.get('/test', (req,res) => {
   return res.json({foo: 'bar'});
 })
 
+app.get('/cats', (req,res) => {
+  return res.json({cat: 'Mr Meows'});
+})
+
+
 server.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
-// ^--------
