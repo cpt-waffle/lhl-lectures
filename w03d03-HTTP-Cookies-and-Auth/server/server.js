13e08918cc3f2a2146756a2ec65d20379fb96858
vasiliy-klimkin <vasily.klimkin@gmail.com>
2023-11-01 12:36:03 -0400

    :books: live lecture added

--- w03d03-HTTP-Cookies-and-Auth/server/server.js
+++ w03d03-HTTP-Cookies-and-Auth/server/server.js
@@ -1,10 +1,19 @@
 const PORT = 8080;
 
-const express = require('express');
-// 1) npm i morgan 
-// 2)
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
 const cookieParser = require('cookie-parser');
+
+
+// config -------------------
+const app = express();
+
+app.use(morgan('dev'));
+app.use(cookieParser())
+// body-parser
+app.use(express.urlencoded({extended: true}));
+
+app.set('view engine', 'ejs');
 
 // user database
 const users = {
@@ -15,98 +24,89 @@ const users = {
 };
 
 
-const app = express();
-// 3)
-app.use(morgan('dev'));
-app.use(express.urlencoded({extended: true})); // body-parser;
-app.use(cookieParser());
 
-app.set('view engine', 'ejs');
 
-app.get('/test', (req, res) => {
-  return res.send("ok!");
-})
 
-app.get('/', (req,res) => {
-  // res.cookie('fruit', 'banana');
-  // res.cookie('fruit', 'apple');
-  // res.cookie('fruit', 'orange');
-  console.log('req.headers.cookie: ',req.headers.cookie);
-  console.log('req.cookies: ',req.cookies);
-  const user_id = req.cookies.user_id; // 1
-  const templateVars = {email: undefined}
-  if (user_id) {
-    templateVars.email = users[user_id].email;
-  }
+app.get('/', (req, res) => {
+  console.log(req.headers.cookie);
+  const user = users[req.cookies.user_id];
+  const templateVars = {user};
+  console.log('Cookies: ', req.cookies)
+  // return res.send('hello world!');
+  // res.cookie('fruit1', 'banana');
+  // res.cookie('fruit2', 'apple');
+  // res.cookie('fruit3', 'orange');
+  // res.cookie('fruit4', 'grapes');
   return res.render('home', templateVars);
 })
 
-
-app.get('/login', (req,res) => {
+app.get('/login', (req, res) => {
   return res.render('login');
 })
 
-// const users = {
-//   1: {id: 1, email: 'obiwan@gmail.com',  password: 'helloThere'},
-//   2: {id: 2, email: 'gimli@gmail.com',   password: 'andMyAxe'},
-//   3: {id: 3, email: 'a@b.com',           password: '123'},
-//   4: {id: 4, email: 'a1@b.com',           password: '123'},
-// };
-
-
-// [x] fill out a form with their credentials
-// [x] get the credials back to the backend server
-// validate those credentials if they exist in our server
-   // -[x] looping through users database
-   // -[x] finding an object that has the same email as the credential given
-   // -[x] checking the password of the object IF that object has matched the credential password 
-// if they valid we have to LOG THEM IN
-// if they are not valid we have to give them AN ERROR
-
 app.post('/login', (req, res) => {
-  console.log("req.body ==>", req.body);
+  console.log(req.body);
+  // loop through our database of users [x]
+  console.log("looping through database of users");
+  for (let key in users) {
+    console.log(users[key].email);
+    // if they match emails, check password [x] 
+    // check if the current user im looping through matches the credentials
+    //given [x]
+    if (users[key].email === req.body.email) {
+      console.log('we found an email match!!');
+      if (users[key].password === req.body.pass) {
+        // if password matches LOG THEM IN [x]
+        console.log('password matches as well');
+        // /1
 
-  const formEmail = req.body.email; 
-  const formPass  = req.body.banana;
-  console.log("------------LOOP-------------")
-  for (key in users) {
-    console.log("key",key);
-    console.log("val", users[key]);
-    console.log("i email", users[key].email);
-    console.log("i password", users[key].password);
-    if (formEmail === users[key].email) {
-      if(formPass === users[key].password) {
-        // return res.send("We should log you in :)");
-        // res.redirect(`${key}/home`);
+        // return res.send("we will log you in right now!");
         res.cookie('user_id', key);
-        return res.redirect('/');
+        return res.redirect(`/`);
       } else {
-        return res.send("Email or password is incorrect :(");
+        // if no email was found once we looped through everything [x]
+        // tell them to go away [x]
+        return res.send('Bad credentials: invalid user or password');
       }
     }
   }
-  console.log("------------LOOP-------------")
-
-  return res.send("Email or password is incorrect :(");
+  return res.send('Bad credentials: invalid user or password');
 })
 
-app.post('/logout', (req,res) => {
+app.post('/logout', (req, res) => {
   res.clearCookie('user_id');
   return res.redirect('/');
 })
 
+app.get('/register', (req, res) => {
+  return res.render('register');
+})
 
+let id = 5;
+app.post('/register', (req, res) => {
+  // loop through our database just to check if email already exists
+  for (let key in users) {
+    if (users[key].email === req.body.email) {
+      // if it does send "they cant register"
+      return res.send("Error: email already exists");
+    }
+  }
+  // if it doesnt add them into the object
+  users[id] = {id, email: req.body.email, password: req.body.pass};
+  // set a cookie
+  res.cookie('user_id', id);
+  id++;
+  // redirect to home page
+  return res.redirect('/');
+})
 
-app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
-
-
-
-// WRONG -----------------Logged in routes
-// app.get('/:user_id/home', (req,res) => {
-//   return res.render('loggedInHome', {username: users[req.params.user_id].email})
+// This is baaad, we dont want to do the code bellow :(
+// app.get('/:id', (req, res) => {
+//   const templateVars = {user: users[req.params.id]};
+//   return res.render('auth_home', templateVars);
 // })
 
-// app.get('/:user_id/stuff', (req,res) => {
 
-// })
-// WRONG -----------------Logged in routes
+
+app.listen(PORT, () => console.log(`Server is on and listening on ${PORT}`));
+
