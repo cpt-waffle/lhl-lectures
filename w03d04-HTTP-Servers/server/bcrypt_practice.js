d5cfcaaacbc91c06225fe4343c756797ff584856
vasiliy-klimkin <vasily.klimkin@gmail.com>
2024-03-07 15:10:49 -0500

    :books: live lecture added

--- w03d04-HTTP-Servers/server/bcrypt_practice.js
+++ w03d04-HTTP-Servers/server/bcrypt_practice.js
@@ -1,27 +1,38 @@
 const bcrypt = require('bcryptjs');
 
+
+console.log("----------------- SYNC Bcrypt Practice  ------------------");
+// console.log(bcrypt);
+// generating salt
+console.log("this is for -------------------------");
 const salt = bcrypt.genSaltSync(10);
+console.log("Salt:    ", salt);
+const hash = bcrypt.hashSync('123', salt);
+console.log("Hash:    ", hash);
+const password = '123456';
+const matches = bcrypt.compareSync(password, hash);
+console.log("does the password match the hash?    ", matches);
+// console.log("this is for 12345 AGAIN -------------------------");
+// const salt1 = bcrypt.genSaltSync(10);
+// console.log("Salt:    ", salt1);
+// const hash1 = bcrypt.hashSync('12345', salt1);
+// console.log("Hash:    ", hash1);
 
-console.log(`Salt:  `, salt);
+console.log('-----------------------------------------------------------');
 
-const hash = bcrypt.hashSync('1234', salt);
+// bcrypt.genSalt(10, (err, salt) => {
+//   console.log("callback finished!");
+//   console.log(salt);
+//   bcrypt.hash('12345', salt, (err, hash) => {
+//     console.log("callback for hash is finished!");
+//     console.log('hash:', hash);
+//   })
+// })
 
-console.log(`Hash:  `, hash);
-
-// Hashing is a ONE WAY PROCESS
-
-
-// To check if plain text string matches the hash
-console.log(bcrypt.compareSync('hell1', hash));
-
-
-console.log(bcrypt);
-
-console.log("---------------- Async -----------------");
-
-bcrypt.genSalt(10, (err, salt) => {
-  console.log("Async Salt: ", salt);
-  bcrypt.hash('hello', salt, (err, hash) => {
-    console.log('Async Hash:  ', hash);
-  })
-})
+// bcrypt.genSalt(10).then(salt => {
+//   console.log("promise has finished for salt");
+//   console.log(salt);
+//   return bcrypt.hash('1234', salt);
+// }).then(hash => {
+//   console.log("hash promise is done", hash);
+// })
--- w03d04-HTTP-Servers/server/server.js
+++ w03d04-HTTP-Servers/server/server.js
@@ -1,14 +1,19 @@
 const PORT = 8080;
 const express = require('express');
 const morgan = require('morgan');
-const bcrypt = require('bcryptjs');
+const cookieParser = require('cookie-parser');
 const cookieSession = require('cookie-session');
+const bcrypt = require('bcryptjs');
+
+// plain text passwords
 
 const users = {
-  1: {id: 1, email: 'obiwan@gmail.com',  password: '$2a$10$51yaLdhAANrv/GbBk8l4b.shX9BHpHN6NCxyYhutXmdHTb8C1IjzK'},
-  2: {id: 2, email: 'gimli@gmail.com',   password: '$2a$10$a6gJ3ehD5uoAKGIP13Z8/eo60YK6634cFwT3izFMXeFUP42JzFNrG'},
-  3: {id: 3, email: 'a@b.ca',           password: '$2a$10$LdGQi9cEzTtvO6.j9JrcieiFRxOzHBL5/APnlLr89WMduVxcyovva'},
+  1: {id: 1, email: 'obiwan@gmail.com',  password: '$2a$10$v.GllU.LRhlRnZY12xeGj.7saDEAOV3FIowojn4AiedvfFaj6Sxnu'},
+  2: {id: 2, email: 'gimli@gmail.com',   password: '$2a$10$zJjZdib5JKXlgtLrsmsQUeOlwUq/D33n91LFDPARFOUXAsd64jmbG'},
+  3: {id: 3, email: 'a@b.com',           password: '$2a$10$nn/lhHacTyhooo2lgLf8UuNGMwyZOwv5hWr83IRWXwk.c2eODDBm2'},
+  4: {id: 4, email: 'a1@b.com',           password: '$2a$10$2N/Fh8btFHjtL2QcjcuRVO8gA/TrFRFwast2DwqBBWPocF1xccysW'},
 };
+
 
 // Index counter for users DB
 let index = 4;
@@ -21,11 +26,16 @@ app.set('view engine', 'ejs');
 // ----> req ---> MIDDLEWARE ---> route  
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended: true}));
+app.use(cookieParser())
+
 app.use(cookieSession({
-  name: 'banana', // what the session is called on the clients browser
+  name: 'session',
   keys: ['one', 'two', 'three'],
+
+  // Cookie Options
   maxAge: 24 * 60 * 60 * 1000 // 24 hours
 }))
+
 
 app.get('/test', (req,res) => {
   res.send("ok!");
@@ -33,13 +43,10 @@ app.get('/test', (req,res) => {
 
 
 app.get('/', (req,res) => {
-  console.log(`Sessions:  `, req.session);
-  // const user_id = req.cookies.user_id; // 1
-  const user_id = req.session.user_id;
+  const user_id = req.session.user_id; // 1
   console.log(user_id);
   console.log(users);
-  console.log('req.cookies: ',req.cookies);
-  const templateVars = {user: undefined}
+  const templateVars = {user: undefined};
   if (user_id) {
     templateVars.user = users[user_id];
     console.log(templateVars);
@@ -57,32 +64,33 @@ app.get('/login', (req, res) => {
 })
 //// POST requests, are here for anytime we want to change/create something on the server
 app.post('/login', (req, res) => {
-  console.log("req.body ==>", req.body);
+  console.log(req.body);
+  // loop through our database of users [x]
+  console.log("looping through database of users");
+  for (let key in users) {
+    console.log(users[key].email);
+    // if they match emails, check password [x] 
+    // check if the current user im looping through matches the credentials
+    //given [x]
+    if (users[key].email === req.body.email) {
+      console.log('we found an email match!!');
+      if (bcrypt.compareSync(req.body.pass, users[key].password)) {
+        // if password matches LOG THEM IN [x]
+        console.log('password matches as well');
+        // /1
 
-  const formEmail = req.body.email; 
-  const formPass  = req.body.pass;
-  console.log("------------LOOP-------------")
-  for (key in users) {
-    console.log("key",key);
-    console.log("val", users[key]);
-    console.log("i email", users[key].email);
-    console.log("i password", users[key].password);
-    if (formEmail === users[key].email) {
-      // if (formPass === users[key].password) {
-      if (bcrypt.compareSync(formPass, users[key].password)) {
-        // return res.send("We should log you in :)");
-        // res.redirect(`${key}/home`);
+        // return res.send("we will log you in right now!");
         // res.cookie('user_id', key);
         req.session.user_id = key;
-        return res.redirect('/');
+        return res.redirect(`/`);
       } else {
-        return res.send("Email or password is incorrect :(");
+        // if no email was found once we looped through everything [x]
+        // tell them to go away [x]
+        return res.send('Bad credentials: invalid user or password');
       }
     }
   }
-  console.log("------------LOOP-------------")
-
-  return res.send("Email or password is incorrect :(");
+  return res.send('Bad credentials: invalid user or password');
 })
 
 app.get('/register', (req, res) => {
@@ -93,40 +101,32 @@ app.get('/register', (req, res) => {
 })
 
 app.post('/register', (req, res) => {
-  for (let id in users) {
-    if (users[id].email === req.body.email)
-      return res.send("Error: email already in use!   :(");
+  // loop through our database just to check if email already exists
+  for (let key in users) {
+    if (users[key].email === req.body.email) {
+      // if it does send "they cant register"
+      return res.send("Error: email already exists");
+    }
   }
-  console.log(req.body);
+  // if it doesnt add them into the object
+
   const salt = bcrypt.genSaltSync(10);
-  const pass = bcrypt.hashSync(req.body.pass, salt);
-  const tempUser = {id: index, email: req.body.email, password: pass};
-  users[index] = tempUser;
-  // res.cookie('user_id', users[index].id);
-  req.session.user_id = users[index].id;
+  const hash = bcrypt.hashSync(req.body.pass, salt);
+
+  users[index] = {id: index, email: req.body.email, password: hash};
+  // set a cookie
+  // res.cookie('user_id', index);
+  // SET a SESSION
+  req.session.user_id = index;
   index++;
+  // redirect to home page
   return res.redirect('/');
 })
-
 
 app.post('/logout', (req, res) => {
   // res.clearCookie('user_id');
   req.session = null;
   return res.redirect('/');
 })
-
-
-///////////////////////////AUTHENTICATED ROUTES /////////////////////
-
-//  WRONG WAY OF DOING AUTHENTICATION 
-
-// app.get('/auth/:id/', (req,res) => {
-//   const templateVars = {user: users[req.params.id].email};
-//   return res.render('auth_home', templateVars);
-// })
-
-
-
-
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
